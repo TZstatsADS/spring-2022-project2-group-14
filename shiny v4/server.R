a973ff0380b8aba130ed835a1cd548fec038e216
idontwantbroccolianymore <94079160+idontwantbroccolianymore@users.noreply.github.com>
2022-02-21 21:31:51 -0500

    Update server.R

--- shiny v4/server.R
+++ shiny v4/server.R
@@ -1,31 +1,25 @@
 server <- function(input, output) {
   
-  # Reactive for produce line chart data for selected county
-  line_data <- reactive({
-    get_line_data(input$selected_dateS, input$selected_dateE,
-                  input$selected_county, input$selected_motive)
-  })
-  
-  
   # Render the line chart
   output$line_plot <- renderPlot({
-    plot_line(line_data(), input$selected_topic)
+    plot_line(input$selected_dateS, input$selected_dateE,
+              input$selected_county, input$selected_motive, input$selected_topic)
   })
   
   # Render the table
   output$info_table <- renderTable({
-    hc_pre_covid %>%
+    tab1 <- hc_pre_covid %>%
       dplyr::select(`Bias Motive Description`, `Full Complaint ID`) %>%
       group_by(`Bias Motive Description`) %>%
-      summarise(cases = length(`Full Complaint ID`))
-    kable(tab1, caption = "Hate crime cases before the Covid-19")
-    
-    hc_since_covid %>%
+      summarise(cases_before_covid = length(`Full Complaint ID`))
+    tab2 <- hc_since_covid %>%
       dplyr::select(`Bias Motive Description`, `Full Complaint ID`) %>%
       group_by(`Bias Motive Description`) %>%
-      summarise(cases = length(`Full Complaint ID`))
-    kable(tab2, caption = "Hate crime cases during the Covid-19")}
+      summarise(cases_during_covid = length(`Full Complaint ID`))
+    print(left_join(tab1, tab2))
+    }
   )
+
     
     
 
